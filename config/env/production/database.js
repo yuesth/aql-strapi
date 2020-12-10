8799080741d618c9a5fb3f6c972065f818e96664
yuesth <mymac@mymacs-MacBook-Pro-2.local>
2020-12-10 22:19:23 +0700

    update database.js move to /config/env/production/database.js

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -8,11 +8,15 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'postgres',
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        username: config.user,
+        password: config.password,
       },
       options: {
-        useNullAsDefault: true,
+        ssl:false,
       },
     },
   },
