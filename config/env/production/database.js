82a730d1d1f3ef7ebe9a3b2cf55d8aa2a7ad1a7f
yuesth <mymac@mymacs-MacBook-Pro-2.local>
2020-12-11 10:55:16 +0700

    udpate database.js 3

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -9,11 +9,11 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: "${process.env.DATABASE_HOST || '127.0.0.1'}",
-        port: "${process.env.DATABASE_PORT || 27017}",
-        database: "${process.env.DATABASE_NAME || 'strapi'}",
-        username: "${process.env.DATABASE_USERNAME || ''}",
-        password: "${process.env.DATABASE_PASSWORD || ''}",
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'aql2db'),
+        username: env('DATABASE_USERNAME', 'aql2db'),
+        password: env('DATABASE_PASSWORD', 'Aql2db123456'),
       },
       options: {
       },
