b2406c61492d48a69e5477499a874783ca5c0a0d
yuesth <mymac@mymacs-MacBook-Pro-2.local>
2020-12-11 10:46:44 +0700

    update database.js 2

--- config/database.js
+++ config/database.js
@@ -1,19 +1,17 @@
-const parse = require('pg-connection-string').parse;
-const config = require(process.env.DATABASE_URL);
-
-
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'postgres',
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'aql2db'),
+        username: env('DATABASE_USERNAME', 'aql2db'),
+        password: env('DATABASE_PASSWORD', 'Aql2db123456'),
       },
-      options: {
-        useNullAsDefault: true,
-      },
+      options: {},
     },
   },
 });
