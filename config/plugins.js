6cc11d9bdb3e6b455184489e2c9161a0f8cd66ad
yuesth <mymac@mymacs-MacBook-Pro-2.local>
2020-12-15 10:28:03 +0700

    cloudinary as upload-provider

---
+++ config/plugins.js
@@ -0,0 +1,12 @@
+module.exports = ({ env }) => ({
+  // ...
+  upload: {
+    provider: 'cloudinary',
+    providerOptions: {
+      cloud_name: "aql-peduli",
+      api_key: "548235898239586",
+      api_secret: "wTvq5tvqQzfeC_uw0Ju8YwLHheE",
+    },
+  },
+  // ...
+});
