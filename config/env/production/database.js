bbfe8ec339a5397b30e57e59f0fd84d9e18af170
yuesth <78777385+yuesmitrasolusi@users.noreply.github.com>
2021-03-14 15:14:41 +0700

    rollback to prev commit

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,5 +1,5 @@
 const parse = require('pg-connection-string').parse;
-const config = parse(process.env.HEROKU_POSTGRESQL_MAROON_URL);
+const config = require(process.env.HEROKU_POSTGRESQL_MAROON_URL);
 
 
 module.exports = ({ env }) => ({
